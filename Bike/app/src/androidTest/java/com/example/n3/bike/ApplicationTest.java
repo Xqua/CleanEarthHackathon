451da129bf354a2e9842d2cbc485919a6008f80c
testifieur <test@local>
2015-04-18 13:19:24 -0400

    First commit

---
+++ Bike/app/src/androidTest/java/com/example/n3/bike/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.n3.bike;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+//import com.google.android.gms.location.ActivityRecognition;
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
